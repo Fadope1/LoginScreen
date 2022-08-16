4bd01f15ed7a7226750fbd11d3c62b692ba1051d
Fabian <fadop3@web.de>
2022-08-16 22:08:31 +0200

    latest

--- lib/utils.dart
+++ lib/utils.dart
@@ -8,73 +8,35 @@ class TestTransform extends StatefulWidget {
 }
 
 class _TestTransformState extends State<TestTransform> {
-  double value = 0;
+  bool isPressed = false;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Center(
-          child: 
-              AnimatedTransform(
-                duration: const Duration(milliseconds: 300),
-                transform: Matrix4.identity()
-                  ..setEntry(3, 2, 0.01)
-                  ..rotateX(
-                    0.01 * value,
-                  )
-                  ..rotateY(
-                    -0.01 * value,
-                  ),
-                child: const Card(
-                  child:
-                      Padding(padding: EdgeInsets.all(20), child: Text("Test")),
+          child: GestureDetector(
+            onTap: () => setState(() {
+              isPressed = !isPressed;
+            }),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.01)
+                ..rotateX(
+                  0.01 * (isPressed ? 0 : 0),
+                )
+                ..rotateY(
+                  -0.01 * (isPressed ? 30 : 0),
                 ),
+              child: const Card(
+                child:
+                    Padding(padding: EdgeInsets.all(20), child: Text("Test")),
               ),
+            ),
+          ),
         ),
       ),
     );
   }
 }
-
-/**
-
-class AnimatedTransform extends ImplicitlyAnimatedWidget {
-  const AnimatedTransform({
-    Key? key,
-    required this.child,
-    required this.transform,
-    required Duration duration,
-    Curve curve = Curves.linear,
-    VoidCallback? onEnd,
-  }) : super(key: key, curve: curve, duration: duration, onEnd: onEnd);
-
-  final Widget child;
-  final Matrix4 transform;
-
-  @override
-  AnimatedWidgetBaseState<AnimatedTransform> createState() =>
-      _AnimatedTransformState();
-}
-
-class _AnimatedTransformState
-    extends AnimatedWidgetBaseState<AnimatedTransform> {
-  Matrix4Tween? _transform;
-
-  @override
-  Widget build(BuildContext context) {
-    return Transform(
-      transform: _transform!.evaluate(animation),
-      child: widget.child,
-    );
-  }
-
-  @override
-  void forEachTween(TweenVisitor visitor) {
-    _transform = visitor(_transform, widget.transform,
-            (dynamic value) => Matrix4Tween(begin: value as Matrix4))
-        as Matrix4Tween?;
-  }
-}
-
- */
