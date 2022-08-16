7c32307a9fd64a4c52b289299076d6aa199bf4b7
fadope1 <fadop3@web.de>
2022-08-16 09:42:19 +0200

    3d card working

--- lib/3dCardEffectPage.dart
+++ lib/3dCardEffectPage.dart
@@ -14,18 +14,21 @@ class _MagicPageState extends State<MagicPage> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onPanUpdate: (details) => setState(() => _offset += details.delta),
-      onDoubleTap: () => setState(() => _offset = const Offset(0.2, 0.6)),
+      onDoubleTap: () =>
+          setState(() => _offset = const Offset(.2, .6)), // reset rotation
       child: Scaffold(
         body: Center(
           child: MovableItem(
             _offset,
-            child: const Card(
+            background: const Card(
               elevation: 15,
+              margin: EdgeInsets.all(50),
               child: Padding(
                 padding: EdgeInsets.all(50.0),
-                child: Material(elevation: 20, child: Text("This is a test")),
+                child: Material(elevation: 20),
               ),
             ),
+            child: const Text("Test"),
           ),
         ),
       ),
@@ -34,29 +37,53 @@ class _MagicPageState extends State<MagicPage> {
 }
 
 class MovableItem extends StatelessWidget {
-  const MovableItem(this._offset, {super.key, this.child});
+  const MovableItem(this._offset,
+      {super.key, required this.child, this.background});
 
   final Offset _offset;
-  final Widget? child;
+  final Widget child;
+  final Widget? background;
 
   @override
   Widget build(BuildContext context) {
-    return Transform(
-      alignment: FractionalOffset.center,
-      transform: Matrix4.identity()
-        ..setEntry(3, 2, 0.01)
-        ..rotateX(
-          0.01 * _offset.dy,
-        )
-        ..rotateY(
-          -0.01 * _offset.dx,
-        ),
-      child: child ??
-          const Card(
-            child: Padding(
-              padding: EdgeInsets.all(20),
+    // make this a stack with background and child seperated 3d effect
+    Offset offset =
+        Offset(_offset.dx.clamp(-100, 100), _offset.dy.clamp(-100, 100));
+    return Stack(
+      children: [
+        Transform(
+          alignment: FractionalOffset.center,
+          transform: Matrix4.identity()
+            ..setEntry(3, 2, 0.01)
+            ..rotateX(
+              0.01 * offset.dy,
+            )
+            ..rotateY(
+              -0.01 * offset.dx,
             ),
+          child: background ??
+              const Card(child: Padding(padding: EdgeInsets.all(20))),
+        ),
+        Positioned(
+          // start position needs to be calculated
+          left: (85 + _offset.dx / 2).clamp(20, 140),
+          top: (85 + _offset.dy / 2).clamp(20, 140),
+          // left: 55 + offset.dx,
+          // top: 60 + offset.dy,
+          child: Transform(
+            alignment: FractionalOffset.center,
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.01)
+              ..rotateX(
+                0.01 * offset.dy,
+              )
+              ..rotateY(
+                -0.01 * offset.dx,
+              ),
+            child: child,
           ),
+        ),
+      ],
     );
   }
 }
